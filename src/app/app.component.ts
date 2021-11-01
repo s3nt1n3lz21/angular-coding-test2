c7a5a786dc3ab0c40d01e67876e7ac8e1591fbdc
Neil Smith <s3nt1n3lz18@gmail.com>
2021-11-01 17:04:06 +0000

    Added a clear button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 import { Country, Region } from './ICountries';
 import { AppState } from './store/app.reducer';
-import { setCountries } from './store/countries.actions';
+import { setCountries, setCountry, setRegion } from './store/countries.actions';
 
 @Component({
   selector: 'app-root',
@@ -41,8 +41,8 @@ export class AppComponent implements OnInit {
     this._apiService.getCountries(region).subscribe((response) => {
       this.countries = response;
       this.countryNames = response.map((country) => country.name.common);
-      // console.log('countryNames: ', this.countryNames);
       this.store.dispatch(setCountries({countries: response}));
+      this.reloadState();
       console.log('response: ', response);
     })
   }
@@ -50,6 +50,8 @@ export class AppComponent implements OnInit {
   selectCountry(countryName: string) {
     console.log(countryName);
     this.selectedCountry = this.countries.filter((c) => c.name.common == countryName)[0];
+    this.store.dispatch(setCountry({country: this.selectedCountry}));
+    this.reloadState();
   }
 
   formatCurrency(currency: Object): string[] {
@@ -59,5 +61,16 @@ export class AppComponent implements OnInit {
       symbols.push(key);
     })
     return symbols;
+  }
+
+  clear() {
+    this.store.dispatch(setCountry({country: null}));
+    this.store.dispatch(setCountries({countries: null}));
+    this.selectedCountry = null;
+    this.reloadState();
+  }
+
+  reloadState() {
+    this.state = this.store.select('state');
   }
 }
